791f23734d17e325fbd50a5c5165cd1ead3f5e47
flestrade <flestrade@spacedev.uy>
2022-06-26 13:48:14 -0300

    feat:ended

--- src/Utils/crypt.js
+++ src/Utils/crypt.js
@@ -13,14 +13,3 @@ export const encryption = (value) => {
         padding: CryptoJS.pad.Pkcs7,
     }).toString();
 };
-
-export const decryption = (value) => {
-    const _key = CryptoJS.enc.Utf8.parse(key);
-    const _iv = CryptoJS.enc.Utf8.parse(iv);
-    return CryptoJS.AES.decrypt(value, _key, {
-        iv: _iv,
-        format: CryptoJS.format.Hex,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-    }).toString(CryptoJS.enc.Utf8);
-};
